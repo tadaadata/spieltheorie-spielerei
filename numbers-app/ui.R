5db6de282c6bb8ab931594a1dc02faf8611f401f
jemus42 <jemus42@quantenbrot.de>
2016-04-12 16:58:48 +0200

    UI is hard

--- numbers-app/ui.R
+++ numbers-app/ui.R
@@ -3,31 +3,29 @@
 # run the application by clicking 'Run App' above.
 #
 # Find out more about building applications with Shiny here:
-# 
+#
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-  
+
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
+  titlePanel("Spieltheoriespielerei"),
+
+  # Sidebar with a slider input for number of bins
+  wellPanel(
+    fluidRow(
+      column(width = 4,
+             numericInput("number", "Zahl zwischen 0 und 10", value = NULL, min = 0, max = 10, width = "100%", step = 1)
+             ),
+      column(width = 4,
+            textInput("id", "ID zur Zuordnung", value = NULL, width = "100%", placeholder = "Test01")
+      ),
+      column(width = 4,
+             tags$strong("Bestätigen"),
+             actionButton("submit", "OK", width = "100%")
+      )
     )
   )
 ))
