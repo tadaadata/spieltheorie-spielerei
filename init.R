3bea6883c16a3eff30e1f97855a643783a37fc40
jemus42 <jemus42@quantenbrot.de>
2016-04-18 14:44:02 +0200

    Doing things? Things have been done.

--- init.R
+++ init.R
@@ -3,3 +3,83 @@
 library(ggplot2)
 if (!("gganimate" %in% installed.packages())) devtools::install_github("dgrtwo/gganimate")
 library(gganimate)
+library(dplyr)
+
+#### Running a simulation ####
+
+play_game <- function(strat_self = "tft", strat_enemy = "soft_majo", turns = 100) {
+  game <- data_frame(self = rep(NA, turns),
+                     enemy = rep(NA, turns),
+                     score = rep(NA, turns))
+
+  for (i in seq(turns)) {
+    if (i == 1) {
+      # first turn
+      if (strat_self %in% c("tft", "soft_majo")) {
+        game[1, "self"] <- TRUE
+        game[1, "enemy"] <- TRUE
+      }
+    } else {
+      if (strat_self == "tft"){
+        game[i, "self"] <- game[i - 1, "enemy"]
+      }
+      if (strat_enemy == "soft_majo"){
+        game[i, "enemy"] <- as.logical(names(table(game$self))[table(game$self) == max(table(game$self))])[1] # soft_majo
+      }
+    }
+  }
+
+  ## Get the score
+  game %>% calc_score() -> game
+
+  return(game)
+}
+
+calc_score <- function(game = NULL) {
+  if (is.null(game)) return(NULL)
+
+  for (i in seq(nrow(game))) {
+    game[i, "score"] <- ifelse(identical(as.logical(game[1, c(1, 2)]), c(TRUE, TRUE)), yes = 3,
+                               ifelse(identical(as.logical(game[1, c(1, 2)]), c(TRUE, FALSE)), yes = 5,
+                                      ifelse(identical(as.logical(game[1, c(1, 2)]), c(FALSE, TRUE)), yes = 0,
+                                             ifelse(identical(as.logical(game[1, c(1, 2)]), c(FALSE, FALSE)), yes = 1, no = NA))))
+  }
+
+  return(game)
+}
+
+
+#### Adaptive strategies ####
+
+# tit for tat
+# kooperiere im ersten Zug,
+# in jedem weiteren Zug spiele den Zug, den der Gegner bei letzten Mal benutzt hat.
+tft <- function(turns) {
+  turns <- seq(turns)
+
+  for (i in turns) {
+    if (i == 1) {
+      x = TRUE
+    } else {
+      x[i] <- games[i-1, 1]
+    }
+  }
+  return(x)
+}
+
+# soft majority; done
+# spiele den meistbenutzten Zug des Gegners, bei Gleichheit kooperiere
+soft_majo <- function(turns) {
+  turns <- seq(turns)
+
+  for (i in turns) {
+    if (i == 1) {
+      x <- TRUE
+    } else {
+      tbl <- prop.table(table(games[1:(i-1), 1]))
+      x[i] <- ifelse(tbl[1] <= 0.5, TRUE, FALSE)
+    }
+  }
+
+  return(x)
+}
